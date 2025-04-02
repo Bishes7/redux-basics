bea9cb342a79cb451f40b7bd4f7a4a1fefb95b1e
Bishes7 <specialbishes9@gmail.com>
2025-04-02 15:29:44 +1100

    redux setup done

---
+++ src/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+
+  reducers: {
+    increase: (state, action) => {
+      state.count = state.count + 1;
+    },
+    decrease: (state, action) => {
+      state.count = state.count - 1;
+    },
+  },
+});
+
+const { reducer, actions } = counterSlice;
+
+export const { increase, decrease } = actions;
+
+export default reducer;
