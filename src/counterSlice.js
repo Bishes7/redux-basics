10824a7ffd69592170efaf2d5f1b5c90c2270471
Bishes7 <specialbishes9@gmail.com>
2025-04-03 01:35:09 +1100

    redux functionality added

--- src/counterSlice.js
+++ src/counterSlice.js
@@ -23,3 +23,32 @@ const { reducer, actions } = counterSlice;
 export const { increase, decrease } = actions;
 
 export default reducer;
+
+
+
+count counterSLice = createSlice({
+    name:"counter",
+    initialState:{
+        count:0
+    },
+
+    reducers:{
+        increase:(state, action)=>{
+            state.count = state.count+1
+        }
+        decrease:(state, action)=>{
+            state.count = state.count-1
+            
+        }
+    }
+})
+
+
+
+const {reducer, action} = counterSlice
+
+export const {}
+
+
+export default reducer
+
