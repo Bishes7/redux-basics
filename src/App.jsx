1b2794deb4e024e29ff0b6b68a4c6eb6f6f9ed26
Bishes7 <specialbishes9@gmail.com>
2025-04-04 18:50:31 +1100

    usedisptach method implemented to update the state

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 import "./App.css";
 import Display from "./Display";
+import { useDispatch } from "react-redux";
+import { increase } from "./counterSlice";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  // Updating the state
+  const dispatch = useDispatch();
+
+  const incr = () => {
+    dispatch(increase());
+  };
 
   return (
     <>
       <Display count={count} />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>+</button>
+        <button onClick={incr}>+</button>
         <button onClick={() => setCount((count) => count - 1)}>-</button>
       </div>
     </>
